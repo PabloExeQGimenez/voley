81dba0f18114a2a672fcaa405d2e664563eed455
Pablo Gimenez <pablogimenez.er@gmail.com>
2025-11-21 01:17:56 -0300

    eslint v9

---
+++ backend/.eslint-config.js
@@ -0,0 +1,24 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['src/**/*.{ts,tsx}'],
+
+    languageOptions: {
+      parser: tsparser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettier.rules,
+    },
+  },
+];
